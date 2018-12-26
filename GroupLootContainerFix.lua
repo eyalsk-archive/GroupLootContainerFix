912abfb69448e4ba46ab851891eacaa6b17fa3de
Eyal Solnik <eyal@solnik.net>
2018-12-26 23:14:17 +0200

    Disable mouse events for GroupLootContainer by default
    I'm not sure whether this a good idea
     but the previous approach didn't seems to fix the problem.

--- GroupLootContainerFix.lua
+++ GroupLootContainerFix.lua
@@ -1,46 +1,16 @@
-local function hide() -- Might not be needed and over-engineered and instead we can simply call :Hide() in all places but needs more testing.
-    if not InCombatLockdown() then
-        GroupLootContainer:Hide()
-    else 
-        GroupLootContainer:EnableMouse(false)
-        C_Timer.After(1, hide)
-    end
-end
+local frame = CreateFrame("Frame")
 
-GroupLootContainer:SetScript("OnShow", function(self)
-    self:EnableMouse(true)
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:SetScript("OnEvent", function(self)
+    GroupLootContainer:EnableMouse(false)
+    frame:UnregisterEvent("PLAYER_LOGIN")
 end)
 
 UIParent:HookScript("OnEvent", function(self, event, ...)
     if event == "SPELL_CONFIRMATION_TIMEOUT" then
 		local _, confirmType = ...
-		if confirmType == LE_SPELL_CONFIRMATION_PROMPT_TYPE_BONUS_ROLL then hide() end
+        if confirmType == LE_SPELL_CONFIRMATION_PROMPT_TYPE_BONUS_ROLL then 
+            if not InCombatLockdown() then GroupLootContainer:Hide() end
+        end
     end
-end)
-
-BonusRollLootWonFrame:HookScript("OnHide", function(self)
-    hide()
-end)
-
-BonusRollMoneyWonFrame:HookScript("OnHide", function(self)
-    hide()
-end)
-
-hooksecurefunc("GroupLootFrame_OnShow", function(self)
-    local _, name = GetLootRollItemInfo(self.rollID)
-	if name == nil then hide() end
-end)
-
-hooksecurefunc("GroupLootFrame_OnEvent", function(self, event, ...)
-	if event == "CANCEL_LOOT_ROLL" then
-		local arg1 = ...;
-		if arg1 == self.rollID then hide() end
-	end
-end)
-
-hooksecurefunc("BonusRollFrame_FinishedFading", function(self)
-    if self.rewardType ~= "item" 
-    and self.rewardType ~= "artifact_power" 
-    and self.rewardType ~= "money" 
-    and self.rewardType ~= "currency" then hide() end
 end)
