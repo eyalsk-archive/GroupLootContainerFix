52db6ed9c78f00e16c1c9d8f42930606bff6cfc0
Eyal Solnik <eyal@solnik.net>
2019-01-05 11:50:39 +0200

    Fix roll frames position and GroupLootContainer size
    Seems like the roll frames aren't being removed properly
     from GroupLootContainer.rollFrames,
     therefor GroupLootContainer continues to grow in size.
    
    I could probably remove the unnecessary frames from
     GroupLootContainer.rollFrames but it might have an effect
     on other stuff so I choose to introduce a workaround.
    
    My assumption is that in any given time
     you can only have a single roll frame up of the same type,
     meaning, you can't have multiple BonusRollFrame up
     but you can have BonusRollFrame and GroupLootFrameX up at the same time.

--- GroupLootContainerFix.lua
+++ GroupLootContainerFix.lua
@@ -1,16 +1,38 @@
 local frame = CreateFrame("Frame")
 
 frame:RegisterEvent("PLAYER_LOGIN")
-frame:SetScript("OnEvent", function(self)
-    GroupLootContainer:EnableMouse(false)
-    frame:UnregisterEvent("PLAYER_LOGIN")
-end)
-
-UIParent:HookScript("OnEvent", function(self, event, ...)
-    if event == "SPELL_CONFIRMATION_TIMEOUT" then
-		local _, confirmType = ...
-        if confirmType == LE_SPELL_CONFIRMATION_PROMPT_TYPE_BONUS_ROLL then 
-            if not InCombatLockdown() then GroupLootContainer:Hide() end
+frame:RegisterEvent("SPELL_CONFIRMATION_PROMPT")
+frame:SetScript("OnEvent", function(self, event, ...)
+    if event == "PLAYER_LOGIN" then
+        GroupLootContainer:EnableMouse(false)
+    elseif event == "SPELL_CONFIRMATION_PROMPT" then
+        local _, confirmType = ...
+        if not InCombatLockdown() and confirmType == LE_SPELL_CONFIRMATION_PROMPT_TYPE_BONUS_ROLL then
+            local indices = {}
+            local maxIndex = 0
+            local container = GroupLootContainer
+            for index, frame in pairs(container.rollFrames) do
+                indices[frame] = index
+                maxIndex = max(maxIndex, index)
+                --print(event, "1st", frame, frame:GetName(), maxIndex, index)
+            end
+            local index = 0
+            for i = 1, maxIndex do
+                local frame = container.rollFrames[i]
+                if i == indices[frame] then
+                    index = index + 1
+                    --print(event, "2nd", frame, frame:GetName(), index, i, container.reservedSize * (index-1 + 0.5))
+                    frame:ClearAllPoints()
+                    frame:SetPoint("CENTER", container, "BOTTOM", 0, container.reservedSize * (index-1 + 0.5))
+                end
+            end
+            --print(event, "3nd", index, container.reservedSize * index)
+            if index > 0 then
+                container:SetHeight(container.reservedSize * index)
+                container:Show()
+            else
+                container:Hide()
+            end
         end
-    end
+    end 
 end)
